54394b38cc511b8beb2eb43d466908e5c8fcdfd5
MikeGarra <michael.garramone@snhu.edu>
2024-01-29 13:40:09 -0500

    Create object Schema with Mongoose, populate MongoDB

---
+++ .seedgooserc.js
@@ -0,0 +1,11 @@
+// .seedgooserc.js
+
+module.exports = {
+    modelBaseDirectory: "app_server/models", //model directory name
+    
+    models: ["*.js", "!db.js"], //model matcher
+
+    data: "data", //data directory name
+
+    db: "mongodb://127.0.0.1:27017/travlr", //db connection url
+};
