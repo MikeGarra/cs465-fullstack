259bf0ba41579ba7e3ff73e469771a7ef56a01a4
MikeGarra <michael.garramone@snhu.edu>
2024-02-13 17:14:10 -0500

    SPA

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -28,7 +28,71 @@ const tripsFindByCode = async (req, res) => {
     });
 };
 
+const tripsAddTrip = async (req, res) => {
+    model
+    .create({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      },
+      (err, trip) => {
+        if (err) {
+          return res
+            .status(400) //bad request
+            .json(err);
+        } else {
+          return res
+            .status(201) //creates
+            .json(trip);
+        }
+    });
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    getUser(req, res, (req, res) => {
+      Model.findOneAndUpdate(
+        { code: req.params.tripCode },
+        {
+          code: req.body.code,
+          name: req.body.name,
+          length: req.body.length,
+          start: req.body.start,
+          resort: req.body.resort,
+          perPerson: req.body.perPerson,
+          image: req.body.image,
+          description: req.body.description,
+        },
+        { new: true }
+      )
+        .then((trip) => {
+          if (!trip) {
+            return res.status(404).send({
+              message: "Trip not found with code " + req.params.tripCode,
+            });
+          }
+          res.send(trip);
+        })
+        .catch((err) => {
+          if (err.kind === "ObjectId") {
+            return res.status(404).send({
+              message: "Trip not found with code " + req.params.tripCode,
+            });
+          }
+          return res
+            .status(500) // server error
+            .json(err);
+        });
+    });
+  };
+
 module.exports = {
     tripsList,
     tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 }
